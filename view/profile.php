c6ee5d32086b9900e82957c692373ab41ff40a3f
carlos55ml <cmoranlozano55@gmail.com>
2022-11-21 13:06:12 +0100

    wip: profile view

---
+++ view/profile.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Perfil - Toorganizer</title>
+  <link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+<?php
+include __DIR__ . '/../modules/header.php';
+if (!$userObj) {
+  header('Location:/login.php');
+}
+
+// pasamos id del usuario que queremos ver por GET
+$id = isset($_GET['id'])?$_GET['id']:null;
+
+// si no pasamos id por GET, esta sera la id del usuario logueado.
+if (is_null($id)) {
+  $id = $userObj['user_id'];
+}
+
+// el usuario a mostrar
+$targetUserObj = fetchUserId($id);
+
+// TODO mostrar datos usuario
+echo "" . $targetUserObj['username'];
+
+?>
+</body>
+</html>

bd3ef963b0a50af4fdd2c6fca38ab4815c5695d5
carlos55ml <cmoranlozano55@gmail.com>
2022-11-22 12:52:52 +0100

    feat: profile view page

--- view/profile.php
+++ view/profile.php
@@ -24,16 +24,27 @@ if (is_null($id)) {
   // el usuario a mostrar
   $targetUserObj = fetchUserId($id);
 }
-
-
-// TODO mostrar datos usuario
-
 ?>
 
 <main>
-  <?php
-  echo $targetUserObj['username'];
-  ?>
+  <?php if(!is_null($targetUserObj)) { ?>
+  <form action="" class="userForm">
+    <figure>
+      <!-- // TODO centrar imagen diabolica diablo papi -->
+      <img src="<?php echo $targetUserObj['avatar_url'];?>" alt="Avatar de usuario" class="user-avatar-view">
+    </figure>
+    <h1>Perfil: <span><?php echo $targetUserObj['username'];?></span></h1>
+    <label for="usernameField" class="col-30">Nombre de usuario: </label>
+    <input type="text" name="usernameField" id="usernameField" class="col-60" disabled value="<?php echo $targetUserObj['username']; ?>">
+    <br>
+    <label for="imgUrl" class="col-30">URL de avatar: </label>
+    <input type="text" name="imgUrl" id="imgUrl" class="col-60" disabled value="<?php echo $targetUserObj['avatar_url'];?>">
+  </form>
+  <?php } else { ?>
+    <form action="" class="userForm">
+    <h1>Usuario no encontrado...</h1>
+  </form>
+  <?php } ?>
 </main>
 </body>
 </html>

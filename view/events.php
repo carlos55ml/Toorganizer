b9ca5c54c09732804a44a4524bea36fe9be185ff
JuandePL <juandediosprunalopez@gmail.com>
2022-12-07 14:20:23 +0100

    feat: event cards view

--- view/events.php
+++ view/events.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,13 +8,14 @@
   <link rel="stylesheet" href="/style.css">
   <title>Eventos - Toorganizer</title>
 </head>
+
 <body>
   <?php
   include_once __DIR__ . "/../modules/header.php";
   include_once __DIR__ . "/../app/eventHandler.php";
 
-  $participant = isset($_GET['participant'])?$_GET['participant']:null;
-  $admin = isset($_GET['admin'])?$_GET['admin']:null;
+  $participant = isset($_GET['participant']) ? $_GET['participant'] : null;
+  $admin = isset($_GET['admin']) ? $_GET['admin'] : null;
 
   $events = null;
 
@@ -27,11 +29,40 @@
   if (!$participant and !$admin) {
     $events = fetchAllEvents();
   }
-  
   ?>
 
   <main>
-    <?php print_r($events) ?>
+    <?php
+    foreach ($events as $event) { ?>
+      <a class="link" href="event.php?eId=<?php echo $event['event_id'] ?>">
+        <div class="tourney-content">
+          <div class="tourney-card">
+            <div class="tourney-info">
+              <img src="<?php echo $event['logo_url'] ?>" />
+              <div class="tourney-data">
+                <h1><?php echo $event['name'] ?></h1>
+                <h3><?php echo $event['game'] ?></h3>
+                <div class="status-owner">
+                  <span class="status">
+                    <span class="circle circle-<?php echo $event['state'] ?>"></span>
+                    <?php
+                    foreach (State::cases() as $case) {
+                      if ($case->key() == $event['state']) {
+                        echo $case->value();
+                        break;
+                      }
+                    }
+                    ?>
+                  </span>
+                  <span class="owner">Owner: <?php echo fetchUserId($event['owner'])['username'] ?></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+    <?php } ?>
   </main>
 </body>
+
 </html>
